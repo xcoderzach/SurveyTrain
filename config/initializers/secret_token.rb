d76f25962fad0c3b9e306d1c65cdd015a6e2551d
Zach Smith <x.coder.zach@gmail.com>
2011-04-14 11:33:54 -0500

    base setup with mongo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveyTrain::Application.config.secret_token = '4c223a5edbf02fa786558bff787eb7e50d3a3abbbe4cd19a14fff5f37aca0a4adba3e011e9fc137de80ffd97d87f39fbc7efc3facb3069de190f4075050dd1c3'
