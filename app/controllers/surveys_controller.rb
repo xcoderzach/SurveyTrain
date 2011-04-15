d19a39238963e7a420ba130d2c7f69c716a9e3ce
Zach Smith <x.coder.zach@gmail.com>
2011-04-15 10:57:29 -0500

    added more methods to survey controller

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -9,11 +9,23 @@ class SurveysController < ApplicationController
     respond_with @survey = Survey.where(:id => params[:id])
   end 
 
+  def new
+    respond_with @survey = Survey.new
+  end 
+ 
   def create
     respond_with @survey = Survey.create(params[:survey])
   end 
 
+  def edit
+    respond_with @survey = Survey.where(:id => params[:id])
+  end 
+
   def update
     respond_with @survey = Survey.update(params[:id], params[:survey])
+  end 
+
+  def delete
+    respond_with @survey = Survey.destroy(:id => params[:id])
   end 
 end 
