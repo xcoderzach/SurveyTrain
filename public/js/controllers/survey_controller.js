4c2f74de31aa8709eeb8f42423e8655a6caf374b
Zach Smith <x.coder.zach@gmail.com>
2011-04-17 20:16:51 -0500

    Code of how I want the controller to be!

--- public/js/controllers/survey_controller.js
+++ public/js/controllers/survey_controller.js
@@ -1,7 +1,21 @@
-var SurveyController = function() {}
+Controller("/surveys", function(survey) {
 
-SurveyController.prototype.show = function() {
-  Survey.first(function(err, survey) {
-    var surveyView = new SurveyView(survey)
+  survey.post(function(params) {
+    Survey.create(params["survey"], function(survey) {
+      var surveyView = new SurveyView(survey)
+    })
   })
+
+  survey.get(function(params) {
+    Survey.all(function(survey) {
+      var surveyView = new SurveyView(survey)
+    })
+  }) 
+
+  survey.get("/:id", function(params) {
+    Survey.first(function(survey) {
+      var surveyView = new SurveyView(survey)
+    })
+  }) 
+
 }
